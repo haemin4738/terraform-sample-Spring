795175aedf6bbda9415052260903b147bc9b4a34
haemin4738 <leeheamin12@gmail.com>
2025-10-28 11:00:21 +0900

    Init

---
+++ src/main/java/com/back/CdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdExampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/back/HomeController.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "<h1>hello</h1>배포 테스트";
+
+    }
+}
